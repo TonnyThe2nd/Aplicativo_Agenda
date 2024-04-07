d8851653c1970122c566293e9ed0a6cba901897a
Antonio Marcos Sousa de Oliveira <150481966+TonnyThe2nd@users.noreply.github.com>
2024-04-07 19:26:11 -0300

    Update AgendarCliente.java

--- src/usuarioADM/AgendarCliente.java
+++ src/usuarioADM/AgendarCliente.java
@@ -15,10 +15,21 @@ import javax.swing.JOptionPane;
 import usuarioCliente.Agendamento;
 
 public class AgendarCliente extends javax.swing.JFrame {
+
+    private javax.swing.JButton CancelarBTTN;
+    private javax.swing.JLabel ClienteLBL;
+    private com.toedter.calendar.JDateChooser DataAgendaDT;
+    private javax.swing.JLabel DataLBL;
+    private javax.swing.JLabel HorarioLBL;
+    private javax.swing.JLabel IMAGEM;
+    private javax.swing.JButton agendarBTTN;
+    private javax.swing.JComboBox<String> horariosDisponiveis;
+    private javax.swing.JTextField nomeCTF;
+    
     public AgendarCliente() {
         initComponents();
     }
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+
     private void initComponents() {
 
         nomeCTF = new javax.swing.JTextField();
@@ -35,72 +46,46 @@ public class AgendarCliente extends javax.swing.JFrame {
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         nomeCTF.setBackground(new java.awt.Color(255, 255, 255));
-        nomeCTF.setFont(new java.awt.Font("Segoe UI Black", 1, 14)); // NOI18N
+        nomeCTF.setFont(new java.awt.Font("Segoe UI Black", 1, 14));
         nomeCTF.setForeground(new java.awt.Color(0, 0, 0));
-        nomeCTF.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                nomeCTFActionPerformed(evt);
-            }
-        });
         getContentPane().add(nomeCTF, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 70, 230, -1));
 
-        ClienteLBL.setFont(new java.awt.Font("Lucida Calligraphy", 1, 18)); // NOI18N
+        ClienteLBL.setFont(new java.awt.Font("Lucida Calligraphy", 1, 18));
         ClienteLBL.setForeground(new java.awt.Color(0, 0, 0));
         ClienteLBL.setText("Cliente");
         getContentPane().add(ClienteLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 70, -1, -1));
 
-        HorarioLBL.setFont(new java.awt.Font("Lucida Calligraphy", 1, 18)); // NOI18N
+        HorarioLBL.setFont(new java.awt.Font("Lucida Calligraphy", 1, 18)); 
         HorarioLBL.setForeground(new java.awt.Color(0, 0, 0));
         HorarioLBL.setText("Horário");
         getContentPane().add(HorarioLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 190, 150, -1));
         getContentPane().add(DataAgendaDT, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 130, -1, -1));
 
         horariosDisponiveis.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "08:00", "09:00", "10:00", "11:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00" }));
-        horariosDisponiveis.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                horariosDisponiveisActionPerformed(evt);
-            }
-        });
         getContentPane().add(horariosDisponiveis, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 190, -1, -1));
 
-        DataLBL.setFont(new java.awt.Font("Lucida Calligraphy", 1, 18)); // NOI18N
+        DataLBL.setFont(new java.awt.Font("Lucida Calligraphy", 1, 18)); 
         DataLBL.setForeground(new java.awt.Color(0, 0, 0));
         DataLBL.setText("Data");
         getContentPane().add(DataLBL, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 130, 150, -1));
 
-        agendarBTTN.setFont(new java.awt.Font("Segoe UI", 3, 18)); // NOI18N
+        agendarBTTN.setFont(new java.awt.Font("Segoe UI", 3, 18)); 
         agendarBTTN.setText("AGENDAR");
-        agendarBTTN.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                agendarBTTNActionPerformed(evt);
-            }
-        });
+        agendarBTTN.addActionListener(evt -> agendarBTTNActionPerformed());
         getContentPane().add(agendarBTTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 300, -1, -1));
 
-        CancelarBTTN.setFont(new java.awt.Font("Segoe UI", 3, 18)); // NOI18N
+        CancelarBTTN.setFont(new java.awt.Font("Segoe UI", 3, 18)); 
         CancelarBTTN.setText("CANCELAR");
-        CancelarBTTN.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                CancelarBTTNActionPerformed(evt);
-            }
-        });
+        CancelarBTTN.addActionListener(evt -> CancelarBTTNActionPerformed());
         getContentPane().add(CancelarBTTN, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 300, -1, -1));
 
-        IMAGEM.setIcon(new javax.swing.ImageIcon("C:\\Users\\Gamer\\Downloads\\UserWLPP.jpg")); // NOI18N
+        IMAGEM.setIcon(new javax.swing.ImageIcon("C:\\Users\\Gamer\\Downloads\\UserWLPP.jpg")); 
         getContentPane().add(IMAGEM, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
-
-    private void nomeCTFActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nomeCTFActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_nomeCTFActionPerformed
-
-    private void horariosDisponiveisActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_horariosDisponiveisActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_horariosDisponiveisActionPerformed
+    }
     //botão de agenda
-    private void agendarBTTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_agendarBTTNActionPerformed
+    private void agendarBTTNActionPerformed() {
         try{
             Connection cn = new ConexaoBancoDeDados().getConnection();
             String cliente = nomeCTF.getText();
@@ -137,11 +122,11 @@ public class AgendarCliente extends javax.swing.JFrame {
         }catch(SQLException e){
             Logger.getLogger(Agendamento.class.getName()).log(Level.SEVERE, null, e);
         }
-    }//GEN-LAST:event_agendarBTTNActionPerformed
+    }
 
-    private void CancelarBTTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CancelarBTTNActionPerformed
+    private void CancelarBTTNActionPerformed() {
        setVisible(false);
-    }//GEN-LAST:event_CancelarBTTNActionPerformed
+    }
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -150,16 +135,4 @@ public class AgendarCliente extends javax.swing.JFrame {
             }
         });
     }
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton CancelarBTTN;
-    private javax.swing.JLabel ClienteLBL;
-    private com.toedter.calendar.JDateChooser DataAgendaDT;
-    private javax.swing.JLabel DataLBL;
-    private javax.swing.JLabel HorarioLBL;
-    private javax.swing.JLabel IMAGEM;
-    private javax.swing.JButton agendarBTTN;
-    private javax.swing.JComboBox<String> horariosDisponiveis;
-    private javax.swing.JTextField nomeCTF;
-    // End of variables declaration//GEN-END:variables
 }
