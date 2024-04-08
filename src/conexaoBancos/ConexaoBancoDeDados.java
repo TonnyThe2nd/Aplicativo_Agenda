bcd3878289992c8f5f1fab14df041ceff0f12319
Antonio Marcos Sousa de Oliveira <150481966+TonnyThe2nd@users.noreply.github.com>
2024-04-08 10:16:34 -0300

    Update ConexaoBancoDeDados.java

--- src/conexaoBancos/ConexaoBancoDeDados.java
+++ src/conexaoBancos/ConexaoBancoDeDados.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package conexaoBancos;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
